8408b6fa613778841808505c97df52437c6a0027
Raman Khurana <khurana@ramans-macbook-pro.home>
2017-08-31 16:27:42 +0200

    adding recent version

--- AUXJMEDQMSummary.C
+++ AUXJMEDQMSummary.C
@@ -35,8 +35,9 @@ using namespace std;
 std::vector<std::string> split(const std::string& text, const std::string& delims);
 
 
-void AUXJMEDQMSummary(TString rootfileList, TString postname){
-   // int colors[] = {1,2,3,4,5,6,7,8};
+void AUXJMEDQMSummary(TString rootfileList, TString postname)
+{
+  // int colors[] = {1,2,3,4,5,6,7,8};
     
   std::cout<<" Now making summary plots for run number provided in the "<<rootfileList<<std::endl;
 
@@ -86,12 +87,14 @@ void AUXJMEDQMSummary(TString rootfileList, TString postname){
 
   TString pdfname;
   for (int ii=0; ii<(int)lines.size(); ii++){
-    
     // Extract the number from the rootfile
     std::vector<std::string> str_component  = split(lines.at(ii),"_");
     std::vector<std::string> str_component_pdf  = split(std::string(postname),"/");
-    pdfname = TString(str_component_pdf[2].c_str()) + "_" + TString(str_component_pdf[3].c_str()) + "_" + TString(str_component_pdf[4].c_str()) +".pdf";
-    
+    if (TString(str_component_pdf[2].c_str()) == "MET") {
+      pdfname = TString(str_component_pdf[2].c_str()) + "_" + TString(str_component_pdf[3].c_str()) + "_" + TString(str_component_pdf[4].c_str()) + "_" + TString(str_component_pdf[5].c_str()) +".pdf";
+    }
+    else pdfname = TString(str_component_pdf[2].c_str()) + "_" + TString(str_component_pdf[3].c_str()) + "_" + TString(str_component_pdf[4].c_str(
+)) +".pdf";
     cout<<pdfname<<endl;
     TString str_component2 = std::string(str_component[2]).c_str();
     TString runnumber = str_component2.ReplaceAll("R000","");
@@ -100,6 +103,8 @@ void AUXJMEDQMSummary(TString rootfileList, TString postname){
     TString prename = "DQMData/Run "+runnumber+"/";
     
     TString histname_ =  prename + postname;
+
+    //std::cout<<'line.at(ii).c_str()'<<line.at(ii)<<std::endl;
     
     // Read the rootfile 
     fin = new TFile(lines.at(ii).c_str());
@@ -122,6 +127,34 @@ void AUXJMEDQMSummary(TString rootfileList, TString postname){
     mean[ii] = h->GetMean();
     RMS[ii]= h->GetRMS();
     
+    double smallmean = pow(2,16);
+    double largemean = -pow(2,16);
+    for (int j = 0; j < nFiles; j++) 
+      {
+	if (mean[j] < smallmean) 
+	  {
+	    smallmean = mean[j];
+	  }
+	if (mean[j] > largemean) 
+	  {
+	    largemean = mean[j];
+	  }
+      }
+
+    double smallRMS = pow(2,16);
+    double largeRMS = -pow(2,16);
+    for (int jj = 0; jj < nFiles; jj++)
+      {
+	if (RMS[jj] < smallRMS)
+	  {
+	    smallRMS = RMS[jj];
+	  }
+	if (RMS[jj] > largeRMS)
+	  {
+	    largeRMS = RMS[jj];
+	  }
+      }
+    
     // h->SetLineColor(colors[ii]);
     
     //if (ii==0) h->DrawNormalized();
@@ -140,68 +173,68 @@ void AUXJMEDQMSummary(TString rootfileList, TString postname){
     
   }
   
+  
+  TCanvas *c1 = new TCanvas("c1","demo bin labels",800,800);
+  c1->SetGrid();
+  c1->SetLeftMargin(0.15);
+  c1->SetBottomMargin(0.15);
+  TH2F *h1 = new TH2F("h1","Mean Values",(nFiles+1)*16,0,(nFiles+1),24000,0.5*smallmean,2*largemean);
+  for (Int_t i=1;i<(nFiles+1);i++) {
+    cout << mean[i] << endl;
+    h1->Fill(i, mean[i-1]);
+  }
+  h1->SetStats(0);
+  h1->GetXaxis()->SetLabelOffset(99);
+  //h1->GetYaxis()->SetLabelOffset(99);
+  h1->SetMarkerStyle(20);
+  h1->Draw("p");
 
-    TCanvas *c1 = new TCanvas("c1","demo bin labels",800,800);
-    c1->SetGrid();
-    c1->SetLeftMargin(0.15);
-    c1->SetBottomMargin(0.15);
-    TH2F *h1 = new TH2F("h1","Mean Values",(nFiles+1)*16,0,(nFiles+1),24000,0,2400);
-    for (Int_t i=1;i<(nFiles+1);i++) {
-      cout << mean[i] << endl;
-      h1->Fill(i, mean[i-1]);
-    }
-    h1->SetStats(0);
-    h1->GetXaxis()->SetLabelOffset(99);
-    //h1->GetYaxis()->SetLabelOffset(99);
-    h1->SetMarkerStyle(20);
-    h1->Draw("p");
+  // draw labels along X
+  Float_t x, y;
+  y = gPad->GetUymin() - 63*h1->GetYaxis()->GetBinWidth(1);
+  TText t;
+  t.SetTextAngle(60);
+  t.SetTextSize(0.02);
+  t.SetTextAlign(33);
+  for (i=1;i<(nFiles+1);i++) {
+    x = h1->GetXaxis()->GetBinCenter(i+1);
+    t.DrawText(i,y,runno[i-1]);
+  }
+  c1->SaveAs("Plots/mean_"+pdfname);
+  c1->SaveAs("Plots/mean_"+pdfname.ReplaceAll(".pdf",".png"));
 
-    // draw labels along X
-    Float_t x, y;
-    y = gPad->GetUymin() - 63*h1->GetYaxis()->GetBinWidth(1);
-    TText t;
-    t.SetTextAngle(60);
-    t.SetTextSize(0.02);
-    t.SetTextAlign(33);
-    for (i=1;i<(nFiles+1);i++) {
-      x = h1->GetXaxis()->GetBinCenter(i+1);
-      t.DrawText(i,y,runno[i-1]);
-    }
-    c1->SaveAs("Plots/mean_"+pdfname);
-    
       
-    TCanvas *c2 = new TCanvas("c2","demoo",800,800);
-    c2->SetGrid();
-    c2->SetLeftMargin(0.15);
-    c2->SetBottomMargin(0.15);
-    TH2F *h2 = new TH2F("h2","RMS",(nFiles+1)*16,0,(nFiles+1),6000,0,600);
-    for (Int_t i=1;i<(nFiles+1);i++) {
-      cout << RMS[i] << endl;
-      h2->Fill(i, RMS[i-1]);
-    }
-    h2->SetStats(0);
-    h2->GetXaxis()->SetLabelOffset(99);
-    //h1->GetYaxis()->SetLabelOffset(99);
-    h2->SetMarkerStyle(20);
-    h2->Draw("p");
-    // draw labels along X
+  TCanvas *c2 = new TCanvas("c2","demoo",800,800);
+  c2->SetGrid();
+  c2->SetLeftMargin(0.15);
+  c2->SetBottomMargin(0.15);
+  TH2F *h2 = new TH2F("h2","RMS",(nFiles+1)*16,0,(nFiles+1),6000,0.5*smallRMS, 2*largeRMS);
+  for (Int_t i=1;i<(nFiles+1);i++) {
+    cout << RMS[i] << endl;
+    h2->Fill(i, RMS[i-1]);
+  }
+  h2->SetStats(0);
+  h2->GetXaxis()->SetLabelOffset(99);
+  //h1->GetYaxis()->SetLabelOffset(99);
+  h2->SetMarkerStyle(20);
+  h2->Draw("p");
+  // draw labels along X
     
     
-    //  Float_t x, y;
-    y = gPad->GetUymin() - 0.2*h1->GetYaxis()->GetBinWidth(1);
-    //TText t;
-    t.SetTextAngle(60);
-    t.SetTextSize(0.02);
-    t.SetTextAlign(33);
-    for (i=1;i<(nFiles+1);i++) {
-      x = h2->GetXaxis()->GetBinCenter(i+1);
-      t.DrawText(i,y,runno[i-1]);
-    }
-    c2->SaveAs("Plots/rms_"+pdfname);
+  //  Float_t x, y;
+  y = gPad->GetUymin() - 0.2*h1->GetYaxis()->GetBinWidth(1);
+  //TText t;
+  t.SetTextAngle(60);
+  t.SetTextSize(0.02);
+  t.SetTextAlign(33);
+  for (i=1;i<(nFiles+1);i++) {
+    x = h2->GetXaxis()->GetBinCenter(i+1);
+    t.DrawText(i,y,runno[i-1]);
+  }
+  //    c2->SaveAs("Plots/rms_"+pdfname);
+  //    c2->SaveAs("Plots/rms_"+pdfname.ReplaceAll(".pdf",".png"));
     
 }
-
-
 
 std::vector<std::string> split(const std::string& text, const std::string& delims)
 {
